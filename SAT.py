30196de1180b278c36b916a9c34bf0c08ae4655d
Victor Ciupec <ciupecvictor@gmail.com>
2020-09-23 13:02:19 +0100

    Add a way to execute all strategies at once - good for experiemntation

--- SAT.py
+++ SAT.py
@@ -3,17 +3,26 @@ from sudoku.solver.sat_solver import solve_all, solve_one
 
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument("-S","--strategy", type=int, choices=range(1,5), default=0, help="The strategy to use: S1, S2 or S3")
+    ap.add_argument("-S","--strategy", type=int, choices=range(0,5), default=-1, help="The strategy to use: S0 (no strategy), S1-S4")
     ap.add_argument("puzzle_filename", type=argparse.FileType('r'), help="the filename containing the SUDOKU puzzle / rules in DIMACS to be solved")
     ap.add_argument("-a", "--all", required=False, action='store_true', help= "If present, solves all the puzzles expressed in dot format present in the file and infers the rules from the size of the rows")
     args = vars(ap.parse_args())
 
-    if args['all']:
-        print("Solving all SUDOKUs using SAT with {} on all {}...".format(args['strategy'],args['puzzle_filename'].name))
-        solve_all(args['strategy'],args['puzzle_filename'])
+    strategy = args['strategy']
+    if strategy == -1:
+        print("No strategy selected - all strategies will be trialed.")
+        for strategy in range(0,5):
+            execute(strategy, args)
     else:
-        print("Solving SUDOKU using SAT with {} on {}...".format(args['strategy'],args['puzzle_filename'].name))
-        solve_one(args['strategy'],args['puzzle_filename'])
+        execute(strategy, args)
+
+def execute(strategy, args):
+    if args['all']:
+        print("Solving all SUDOKUs using SAT with {} on all {}...".format(strategy,args['puzzle_filename'].name))
+        solve_all(strategy,args['puzzle_filename'])
+    else:
+        print("Solving SUDOKU using SAT with {} on {}...".format(strategy,args['puzzle_filename'].name))
+        solve_one(strategy,args['puzzle_filename'])
 
 if __name__ == "__main__":
     main()
